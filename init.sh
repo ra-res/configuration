c1a39a8328aaa2b3a4c1b44cbebddaafc524663b
nifty <purtan.rares@yahoo.com>
2021-09-12 13:39:01 +0100

    Basic init script for dotfiles

---
+++ init.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# vim
+cp .vimrc ~/.vimrc
+
+# bash
+cp .bashrc ~/.bashrc
+cp .bash_profile ~/.bash_profile
+cp .inputrc ~/.inputrc
+
+# zsh
+cp .zshrc ~/.zshrc
+
+# tmux 
+cp .tmux.conf ~/.tmux.conf
+
+# intellij
+cp .ideavimrc ~/.ideavimrc
